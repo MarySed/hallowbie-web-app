42ebb5e14b3dc0248d74ba8a5874be0dfa6f5069
Mary Sedarous <mary@MacBook-Pro.local>
2020-10-28 21:18:15 +0900

    feat: zombie head one realness

--- components/Heads/ZombieHeadOne.jsx
+++ components/Heads/ZombieHeadOne.jsx
@@ -1,25 +1,51 @@
-import React, { useRef, useState } from 'react';
-import { useLoader, useFrame } from 'react-three-fiber';
+import React, { useRef } from 'react';
+import { useLoader } from 'react-three-fiber';
 let GLTFLoader;
 
 const Skull = () => {
     GLTFLoader = require('three/examples/jsm/loaders/GLTFLoader').GLTFLoader;
 
-    const skullRef = useRef();
+    const zombieHeadRef = useRef();
 
     const { nodes } = useLoader(GLTFLoader, '/models/ZombieHead.glb');
 
-    console.log(nodes);
-
     return (
-        <group>
-            <mesh>
-                {/* <meshStandardMaterial
+        <group ref={zombieHeadRef}>
+            <mesh
+                visible
+                geometry={nodes['Base'].geometry}
+                position={nodes['Base'].position}
+                scale={nodes['Base'].scale}>
+                <meshStandardMaterial
                     attach="material"
-                    color="white"
+                    color="green"
                     roughness={0.1}
                     metalness={0.1}
-                /> */}
+                />
+            </mesh>
+            <mesh
+                visible
+                geometry={nodes['Left_Eye'].geometry}
+                position={nodes['Left_Eye'].position}
+                scale={nodes['Left_Eye'].scale}>
+                <meshStandardMaterial
+                    attach="material"
+                    color="red"
+                    roughness={0.1}
+                    metalness={0.1}
+                />
+            </mesh>
+            <mesh
+                visible
+                geometry={nodes['Right_Eye'].geometry}
+                position={nodes['Right_Eye'].position}
+                scale={nodes['Right_Eye'].scale}>
+                <meshStandardMaterial
+                    attach="material"
+                    color="red"
+                    roughness={0.1}
+                    metalness={0.1}
+                />
             </mesh>
         </group>
     );
